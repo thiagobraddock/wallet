d859d3773053509f9f6be055be6b2770cd94f202
joaoAndradeJr <fullservicemanaus@gmail.com>
2024-05-23 10:12:20 -0300

    feat: req 02

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
+import { useSelector } from 'react-redux';
+import { GlobalState } from '../types';
+
 function Header() {
-  return <div>Header</div>;
+  const email = useSelector((globalState: GlobalState) => globalState.user.email);
+  return (
+    <>
+      <h2 data-testid="email-field">{ email }</h2>
+      <h3 data-testid="header-currency-field">
+        BRL
+        {' '}
+        <span data-testid="total-field">0</span>
+      </h3>
+    </>
+  );
 }
 
 export default Header;
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,7 @@
+import Header from '../components/Header';
+
 function Wallet() {
-  return <div>TrybeWallet</div>;
+  return <Header />;
 }
 
 export default Wallet;
